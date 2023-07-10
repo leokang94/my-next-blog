9cfd6c47144637c3ae3e9510b977fe12e0cb6f80
leokang94 <acccdang8659@gmail.com>
2023-07-10 22:11:24 +0900

    imp: markdown syntax highlight 적용

--- __posts/first.md
+++ __posts/first.md
@@ -4,3 +4,17 @@ date: 2023-07-09
 ---
 
 ## Hello
+
+`name` 은 이름입니다.
+
+```javascript
+console.log('hello!');
+```
+
+```tsx
+import React from 'react';
+
+export default function Test() {
+  return <div>Test Component!</div>;
+}
+```
--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,15 +1,24 @@
-import remarkHtml from 'remark-html';
+import rehypeFormat from 'rehype-format';
+import rehypeRaw from 'rehype-raw';
+import rehypeStringify from 'rehype-stringify';
 import remarkParse from 'remark-parse';
-import { Plugin, unified } from 'unified';
+import remarkPrism from 'remark-prism';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 
 /**
  * @reference https://github.com/vercel/next.js/blob/canary/examples/blog-starter/lib/markdownToHtml.ts
+ * @reference syntax-highlighting with prism: https://ahmadrosid.com/blog/using-remark-prism-nextjs
  */
 
 export async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
-    .use(remarkHtml as Plugin)
+    .use(remarkPrism)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
     .process(markdown);
 
   return result.toString();
